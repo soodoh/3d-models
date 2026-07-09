50f6c162d3596dc8457cd842bc61e1ca560b0fdd
Paul DiLoreto <soodohh@pm.me>
2026-07-08 22:40:07 -0700

    fix: dutch blitz back lid dovetail support

--- src/print_models/models/dutch_blitz_card_storage_box_parametric.py
+++ src/print_models/models/dutch_blitz_card_storage_box_parametric.py
@@ -430,13 +430,30 @@ def _right_lid_track_cut(
 
 def _top_click_features(*, cq, outer_width: float, outer_depth: float, track_z: float):
     track_center_x = outer_width / 2.0 - 2.0
-    frame_outer = cq.Workplane("XY").box(119.0, 62.588612, 0.2).translate(
-        (2.0, 0.0, track_z - 0.1)
+    frame_outer_width = outer_width - 4.0
+    frame_outer_depth = outer_depth - 3.411388
+    frame_inner_width = outer_width - 10.725808
+    frame_inner_depth = outer_depth - 7.0
+    frame_center_x = 2.0
+    inner_center_x = 0.0
+    frame_outer = cq.Workplane("XY").box(frame_outer_width, frame_outer_depth, 0.2).translate(
+        (frame_center_x, 0.0, track_z - 0.1)
     )
-    frame_inner = cq.Workplane("XY").box(112.274192, 59.0, 0.4).translate(
-        (0.0, 0.0, track_z - 0.1)
+    frame_inner = cq.Workplane("XY").box(frame_inner_width, frame_inner_depth, 0.4).translate(
+        (inner_center_x, 0.0, track_z - 0.1)
     )
     track_floor = frame_outer.cut(frame_inner)
+
+    back_support_outer_x = frame_center_x - frame_outer_width / 2.0
+    back_support_inner_x = inner_center_x - frame_inner_width / 2.0
+    back_support_width = back_support_inner_x - back_support_outer_x
+    back_support_center_x = back_support_outer_x + back_support_width / 2.0
+    back_support = cq.Workplane("XY").box(
+        back_support_width,
+        frame_outer_depth,
+        track_z,
+    ).translate((back_support_center_x, 0.0, track_z / 2.0))
+
     lug = (
         cq.Workplane("XY", origin=(0.0, 0.0, track_z))
         .center(track_center_x, 0.0)
@@ -448,7 +465,7 @@ def _top_click_features(*, cq, outer_width: float, outer_depth: float, track_z: 
     except Exception:
         pass
 
-    return track_floor.union(lug)
+    return track_floor.union(back_support).union(lug)
 
 
 def _engrave_container_side_logos(
